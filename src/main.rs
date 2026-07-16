84cdd0b9d220411cdb62366a57d8f7bc28908100
Sam Reynolds <sreyn@proton.me>
2026-07-16 08:36:36 -0700

    remove context manager

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use fix_affix::{ContextManager, HunspellDict};
+use fix_affix::HunspellDict;
 use std::env::args;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let args: Vec<String> = args().collect();
 
-    let ctx: ContextManager = ContextManager::new(args);
+    if args.len() < 2 {
+        panic!("Please supply input file");
+    }
 
-    let dict: HunspellDict = match HunspellDict::load_from_toml_file(&ctx.toml_file) {
+    let toml_file: PathBuf = Path::new(&args[1]).to_owned();
+    let aff_file: PathBuf = toml_file.with_extension("aff");
+    let dic_file: PathBuf = toml_file.with_extension("dic");
+
+    let dict: HunspellDict = match HunspellDict::load_from_toml_file(&toml_file) {
         Ok(data) => data,
-        Err(e) => panic!("TOML dictionary not loaded ({:?}): {}", &ctx.toml_file, e),
+        Err(e) => panic!("TOML dictionary not loaded ({:?}): {}", &toml_file, e),
     };
 
-    if let Err(e) = dict.write_dic_file(&ctx.dic_file) {
+    if let Err(e) = dict.write_dic_file(&dic_file) {
         panic!("Failed to build Hunspell dic: {}", e)
     };
 
-    if let Err(e) = dict.write_aff_file(&ctx.aff_file) {
+    if let Err(e) = dict.write_aff_file(&aff_file) {
         panic!("Failed to build Hunspell aff: {}", e)
     };
 }
